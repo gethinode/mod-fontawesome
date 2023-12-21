91541c02d71aa2e30e5e423c1a016db97ed3749c
mark <mark@dumay.nl>
2023-12-21 13:37:29 +0100

    fix: do not set empty style
    Avoids a CSP error when inline styles are not permitted, even when the provided style is empty.

--- assets/js/modules/fontawesome/fontawesome.dynamic.js
+++ assets/js/modules/fontawesome/fontawesome.dynamic.js
@@ -1,23 +1,25 @@
 document.querySelectorAll('.dynamic-svg').forEach((placeholder) => {
-    placeholder.onload = function () {
-      const container = placeholder.parentElement
-      const doc = placeholder.contentDocument
-      const attr = placeholder.getAttribute('data-class')
-      const style = placeholder.getAttribute('data-style')
-      if (container !== null && doc !== null) {
-        const svg = doc.querySelector('svg')
-        if (svg !== null) {
-          svg.setAttribute('class', 'svg-inline--fa ' + (attr || ''))
-          svg.setAttribute('fill', 'currentcolor')
-          svg.setAttribute('aria-hidden', 'true')
-          svg.setAttribute('role', 'img')
+  placeholder.onload = function () {
+    const container = placeholder.parentElement
+    const doc = placeholder.contentDocument
+    const attr = placeholder.getAttribute('data-class')
+    const style = placeholder.getAttribute('data-style')
+
+    if (container !== null && doc !== null) {
+      const svg = doc.querySelector('svg')
+      if (svg !== null) {
+        svg.setAttribute('class', 'svg-inline--fa ' + (attr || ''))
+        svg.setAttribute('fill', 'currentcolor')
+        svg.setAttribute('aria-hidden', 'true')
+        svg.setAttribute('role', 'img')
+        if (style !== null && style !== '') {
           svg.setAttribute('style', style)
-          svg.removeAttribute('height')
-          svg.removeAttribute('width')
-          container.innerHTML = ''
-          container.appendChild(svg)
         }
+        svg.removeAttribute('height')
+        svg.removeAttribute('width')
+        container.innerHTML = ''
+        container.appendChild(svg)
       }
     }
-  })
-  
+  }
+})
